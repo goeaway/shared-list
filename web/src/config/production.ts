bb4aa6d60c5da5c62a99a27f5abae7bd7f4ed1a3
Joe Thompson <joseph.thompson>
2020-05-26 21:10:55 +0100

    changed api url for prod

--- web/src/config/production.ts
+++ web/src/config/production.ts
@@ -1,4 +1,4 @@
 export const config = {
-    apiURL: "https://api.sharethe.shop",
+    apiURL: "http://sharedlist-api.azurewebsites.net",
     googleAuthClientId: "787759781218-jr34v4kr0ht47lhicb1bon3jbu3sd9g0.apps.googleusercontent.com",
 }
