00aa3b00e2c9db0eef67bda653f3a80ffbb0bb48
Joe Thompson <joseph.thompson>
2020-05-31 16:20:57 +0100

    updated front end config for api url to bypass DNS issues for the time being

--- web/src/config/production.ts
+++ web/src/config/production.ts
@@ -1,5 +1,5 @@
 export const config = {
-    apiURL: "https://api.sharethe.shop",
+    apiURL: "https://x3nygjtwqe.execute-api.eu-west-2.amazonaws.com/live",
     googleAuthClientId: "787759781218-jr34v4kr0ht47lhicb1bon3jbu3sd9g0.apps.googleusercontent.com",
     limits: {
         lists: 10,
