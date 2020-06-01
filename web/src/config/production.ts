d1eb32b29f089a5ca185442790e3d2e2b9293847
Joe Thompson <joseph.thompson>
2020-06-01 14:03:44 +0100

    changed front end config for api url again

--- web/src/config/production.ts
+++ web/src/config/production.ts
@@ -1,5 +1,5 @@
 export const config = {
-    apiURL: "https://dzokm1g9te.execute-api.eu-west-2.amazonaws.com",
+    apiURL: "https://sharelistapi-env.eba-gghhs3sq.eu-west-2.elasticbeanstalk.com",
     googleAuthClientId: "787759781218-jr34v4kr0ht47lhicb1bon3jbu3sd9g0.apps.googleusercontent.com",
     limits: {
         lists: 10,
