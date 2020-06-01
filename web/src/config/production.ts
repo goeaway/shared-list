917477adb604b3ff7367e8d8fd7307e627f7c3a0
Joe Thompson <joseph.thompson>
2020-06-01 09:46:10 +0100

    updated front end config for new API base url

--- web/src/config/production.ts
+++ web/src/config/production.ts
@@ -1,5 +1,5 @@
 export const config = {
-    apiURL: "https://x3nygjtwqe.execute-api.eu-west-2.amazonaws.com/live",
+    apiURL: "https://dzokm1g9te.execute-api.eu-west-2.amazonaws.com",
     googleAuthClientId: "787759781218-jr34v4kr0ht47lhicb1bon3jbu3sd9g0.apps.googleusercontent.com",
     limits: {
         lists: 10,
